276e06d84acae610ee0f96ac43d1bf6e8d7ee3a4
Georg Schmidt <gs-develop@gs-sys.de>
2019-01-06 22:03:49 +0100

    release 0.2

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ if [ ! -d ~/bin/ ]
 then
   mkdir ~/bin/
 fi
-wget -O ~/bin/lsOktalACL.class https://github.com/user9209/show-octal-acls-in-ls/releases/download/0.1/lsOktalACL.class
+wget -O ~/bin/lsOktalACL.class https://github.com/user9209/show-octal-acls-in-ls/blob/master/release/lsOktalACL.class
 
 echo >> ~/.bashrc "alias ls='ls -A -l -h --color=auto | java -cp ~/bin/ lsOktalACL'"
